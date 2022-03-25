c61be95b003bce5c339cdf44d0301768aea04f0e
MDA-2 <94649472+MDA-2@users.noreply.github.com>
2022-03-24 22:18:29 -0500

    Add files via upload

---
+++ firefoxClone.py
@@ -0,0 +1,8 @@
+#python firefox clone -- not working in windows because we all hate windows
+#1000+ lines of code
+#1 year and a half of development
+#20+ issues resolved
+#1.02
+
+import os
+os.system("firefox")
